0256d5c5576f28dd44eea71b011909bed1ee2284
ritztoston <ritztoston@gmail.com>
2019-07-09 20:08:15 +0800

    Added maxar account, fixed adding of subscribers

--- src/utils/archintelData.js
+++ src/utils/archintelData.js
@@ -9,11 +9,14 @@ const data = [
     createData('AECOM', 'aecom', 'Active'),
     createData('Leidos', 'leidos', 'Active'),
     createData('SOS International', 'sosi', 'Active'),
+    // createData('Iridium', 'iridium', 'Not Active'),
     createData('Perspecta', 'perspecta', 'Active'),
+    createData('Maxar Technologies', 'maxar', 'Internal Testing'),
     createData('CACI', 'caci', 'Active'),
     createData('Premier', 'premier', 'Active'),
     createData('George Washington University', 'george', 'Active'),
     createData('SAP Public Services', 'sap', 'Internal Testing'),
+    createData('KBR', 'kbr', 'Internal Testing'),
 ];
 
 export default data;
